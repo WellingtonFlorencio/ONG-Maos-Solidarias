153c987741291fa4488e8ccdcb829869d7063238
Dev Newbie <107278960+WellingtonFlorencio@users.noreply.github.com>
2025-11-01 17:44:58 -0300

    Add files via upload

---
+++ Entrega I/assets/js/script.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded', function() {
+    // Aqui você adicionará as funções de JS nas próximas entregas.
+    
+    console.log("Plataforma ONG: JS carregado com sucesso!");
+});
